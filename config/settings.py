116f6c9a99456b4e52e25e38b861eb5ff906f518
oybekjohn <oybeksjob@gmail.com>
2022-06-06 01:32:45 +0500

    some fixed

--- config/settings.py
+++ config/settings.py
@@ -90,18 +90,22 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "TUIT_CRM",
-        "USER": "postgres",
-        "PASSWORD": "123",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
+
+
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql_psycopg2",
+    #     "NAME": "tatu",
+    #     "USER": "postgres",
+    #     "PASSWORD": "root",
+    #     "HOST": "localhost",
+    #     "PORT": "5432",
+    # }
+
+
     # 'default': env.dj_db_url("DATABASE_URL")
 }
 
